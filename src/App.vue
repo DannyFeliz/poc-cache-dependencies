be2ee55020b7cd36dba59c5c27dbe5c9b3fdec98
Danny Feliz <dannyfeliz08@gmail.com>
2021-02-17 12:28:53 -0400

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <Counter />
+</template>
+
+<script lang="ts">
+import { defineAsyncComponent, defineComponent } from 'vue'
+
+export default defineComponent({
+  name: 'App',
+  components: {
+    Counter: defineAsyncComponent(() => import('./components/Counter.vue'))
+  }
+})
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/Counter.vue
@@ -0,0 +1,33 @@
+<template>
+  <div>Hello {{ name }}</div>
+  {{ fullName }}
+  <div>
+    {{ log }}
+  </div>
+</template>
+
+<script setup lang="ts">
+import type { Ref } from "vue";
+import { ref, computed, watch } from "vue";
+
+const name = ref("John");
+const fullName = computed(() => {
+  return name.value + " Doe";
+})
+
+watch(name, () => {
+  log.value = "Name changed to: " + name.value;
+})
+
+const log: Ref<string> = ref("");
+
+setTimeout(() => {
+  name.value = "Maria";
+}, 2000)
+
+
+setTimeout(() => {
+  name.value = "Pedro";
+}, 3000)
+
+</script>
